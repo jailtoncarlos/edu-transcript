53e14dfea2f95f4213831891e99da373527435ea
Jailton Paiva <jailtoncarlos@gmail.com>
2026-03-17 00:44:08 -0300

    Initialize edu_transcript module with transcriptors

---
+++ edu_transcript/__init__.py
@@ -0,0 +1,5 @@
+from .base import UolEdtechTranscriptor
+from .fsl import FSLTranscriptor
+from .uninassau import UninassauTranscriptor
+
+__all__ = ["UolEdtechTranscriptor", "FSLTranscriptor", "UninassauTranscriptor"]
